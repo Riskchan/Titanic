d216c179bd436c423971da333671de97d8cee704
Akihiko Minami <minamiakihiko@gmail.com>
2019-03-31 09:44:01 +0900

    Cleanup

--- titanic_singlelayer.py
+++ titanic_singlelayer.py
@@ -2,25 +2,24 @@ import numpy as np
 import pandas as pd
 import tensorflow as tf
 import sklearn.metrics as metrics
+from sklearn import preprocessing
 
 # データを読み込み、必要なものだけを取り出す
 row_data = pd.read_csv('train.csv')
 data= row_data[['Survived','Pclass','Sex','Age','SibSp','Parch','Fare']].dropna()
+cols = len(data.columns)
 
-#カテゴリーデータをダミー変数に変換し、使用するものだけにする
-Sex = pd.get_dummies(data['Sex'])
-Sex = Sex.drop('male',axis=1)
+# Encode Sex and Embarked
+labelEncoder = preprocessing.LabelEncoder()
+data['Sex'] = labelEncoder.fit_transform(data['Sex'])
 
-Data_tmp = data[['Survived','SibSp','Parch','Fare', 'Age', 'Pclass']]
-
-Merge_data = pd.merge(Data_tmp,Sex,right_index=True,left_index=True)
-y = Merge_data['Survived'].values.reshape(-1, 1)
-x = Merge_data.drop('Survived',axis=1).values
+y = data['Survived'].values.reshape(-1, 1)
+x = data.drop('Survived',axis=1).values
 
 # 変数の定義
-feature = tf.placeholder(tf.float32, [None, 6])
+feature = tf.placeholder(tf.float32, [None, cols-1])
 label = tf.placeholder(tf.float32, [None, 1])
-w0 = tf.Variable(tf.zeros([6, 1]))
+w0 = tf.Variable(tf.zeros([cols-1, 1]))
 b0 = tf.Variable(tf.zeros([1]))
 
 # モデルの定義
@@ -35,7 +34,7 @@ optimizer = tf.train.AdamOptimizer().minimize(loss)
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 
-for i in range(5000):
+for i in range(10000):
     sess.run(optimizer, feed_dict={feature:x, label:y})  # 学習処理
     if i % 100 == 0:
         loss_val = sess.run(loss, feed_dict={feature:x, label:y})
